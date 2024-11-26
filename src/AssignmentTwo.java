6c482cdc64eb9f1b3b7a6af2c615cd00597a0e56
XunJH <163519545+XunJH@users.noreply.github.com>
2024-11-26 22:00:28 +0800

    First submission
    Create a main class of AssignmentTwo in the src directory, containing the main method and placeholders for the other partnamber methods. Define the other four classes

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,3 +1,12 @@
 public class AssignmentTwo {
+    public static void main(String[] args) {
+        System.out.println("Theme Park Ride Management System!");
+    }
 
+    public void partThree() {}
+    public void partFourA() {}
+    public void partFourB() {}
+    public void partFive() {}
+    public void partSix() {}
+    public void partSeven() {}
 }
