89c8aecd190d81878985dbe440394db2272e2395
XunJH <163519545+XunJH@users.noreply.github.com>
2024-11-28 22:38:51 +0800

    Third submission
    Create the RideInterface interface and modify the Ride class to implement the RideInterface

--- src/Ride.java
+++ src/Ride.java
@@ -1,14 +1,63 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Iterator;
 
-public class Ride {
+public class Ride implements RideInterface {
     private String rideName; // Name of amusement facilities
     private int maxRider; // Maximum number of passengers per ride
     private Employee operator; // operator
     private Queue<Visitor> visitorQueue; // Waiting queue
+    private LinkedList<Visitor> rideHistory;
 
     public Ride() {
         visitorQueue = new LinkedList<>();
+        rideHistory = new LinkedList<>();
+    }
+
+    @Override
+    public void addVisitorToQueue(Visitor visitor) {
+        visitorQueue.add(visitor);
+    }
+
+    @Override
+    public void removeVisitorFromQueue() {
+        visitorQueue.poll();
+    }
+
+    @Override
+    public void printQueue() {
+        for (Visitor visitor : visitorQueue) {
+            System.out.println(visitor.getName());
+        }
+    }
+
+    @Override
+    public void addVisitorToHistory(Visitor visitor) {
+        rideHistory.add(visitor);
+    }
+
+    @Override
+    public boolean checkVisitorFromHistory(Visitor visitor) {
+        return rideHistory.contains(visitor);
+    }
+
+    @Override
+    public int numberOfVisitors() {
+        return rideHistory.size();
+    }
+
+    @Override
+    public void printRideHistory() {
+        Iterator<Visitor> iterator = rideHistory.iterator();
+        while (iterator.hasNext()) {
+            Visitor visitor = iterator.next();
+            System.out.println(visitor.getName());
+        }
+    }
+
+    @Override
+    public void runOneCycle() {
+        // Implementation logic
     }
 
     public Ride(String rideName, int maxRider, Employee operator) {
